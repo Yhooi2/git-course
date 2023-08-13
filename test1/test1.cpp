07c6c0173d3dab5c9e9fd560d7053e030978a572
Yhoo2 <yhooi2011@gmail.com>
2023-08-13 20:14:57 +0300

    lesson 4 struct v0

--- test1/test1.cpp
+++ test1/test1.cpp
@@ -2,12 +2,15 @@
 #include <string>]
 
 using namespace std;
+struct Animals {
+	int legs = 4;
+	string type = "";
+	string name = "";
+};
 
 int main() {
-	string name = "135";
-	int x = 345;
-	int y = stoi(name);
-	int z = x + y;
-	name = to_string(x) + "00";
-	cout << y+1 << endl << name << endl << z;
+	Animals cat;
+	cat.type = "britanec";
+	cat.name = "Barsic";
+	cout << cat.name	
 }
