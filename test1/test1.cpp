a97b215cd8f23b23bfe8bc20ef426ec7177d4f48
Yhoo2 <yhooi2011@gmail.com>
2023-08-13 17:39:57 +0300

    lesson 2 finish

--- test1/test1.cpp
+++ test1/test1.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
-#include <string>
+#include <string>]
+#include <iomanip>
 
 using namespace std;
 
 int main() {
-	char x = 'x';
-	cout << x << ' ' << int(x);
+	float f = 1.55, f2;
+	int x, y=2;
+	x = static_cast<int> (f);
+	f2 = int(y);
+	cout << float(x) << endl << f2  << endl;
+	cout << fixed;
+	cout << setprecision(8) << f;
+	
 }
