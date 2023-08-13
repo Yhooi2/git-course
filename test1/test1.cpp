0f2a776c0ed5a67913117b437fb27d690be9d2d9
Yhoo2 <yhooi2011@gmail.com>
2023-08-13 18:20:40 +0300

    lesson 3

--- test1/test1.cpp
+++ test1/test1.cpp
@@ -1,16 +1,10 @@
 #include <iostream>
 #include <string>]
-#include <iomanip>
 
 using namespace std;
 
 int main() {
-	float f = 1.55, f2;
-	int x, y=2;
-	x = static_cast<int> (f);
-	f2 = int(y);
-	cout << float(x) << endl << f2  << endl;
-	cout << fixed;
-	cout << setprecision(7) << f;
-	
+	string name = "Alex";
+	char c = name[1];
+	cout << name[0] << c << endl <<name.size();
 }
