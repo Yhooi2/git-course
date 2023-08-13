1967297ea3ba7adb0b654372cbebb8e7c1b0d335
Yhoo2 <yhooi2011@gmail.com>
2023-08-13 20:48:36 +0300

    const

--- test1/test1.cpp
+++ test1/test1.cpp
@@ -2,18 +2,10 @@
 #include <string>]
 
 using namespace std;
-struct Animals {
-	int legs = 4;
-	string type = "";
-	string name = "";
-};
-struct Position {
-	double x = 0, y = 0;
-};
 
+const float PI = 3.14;
 int main() {
-	Animals cat = { 4,"britanec", "Barsic" };
-	Position Moscow = { 100.5, 100.2 }, London = { 150.1, 300.6 };
-	cout << cat.legs << ' ' << cat.type << ' ' << cat.name << endl;
-	cout << Moscow.y << ' ' << London.y << endl << sizeof(Moscow);
+	// ...
+	
+	cout << PI;
 }
