19cb4f80643b8de1d3d2118f3f08c65a091e9e15
Yhoo2 <yhooi2011@gmail.com>
2023-08-15 13:09:42 +0300

    Lesson 8.0 Chess Bishop's coordinates

--- C++/Lesson4/Lesson4.cpp
+++ C++/Lesson4/Lesson4.cpp
@@ -3,48 +3,9 @@
 using namespace std;
 
 int main() {
-	int x, y, z;
-	cin >> x >> y >> z;
-	int mx = max(x, max(y, z));
-	int mn = min(x, min(y, z));
-	// int md = x + y + z - mx - mn
+	int x, y;
+	cin >> x >> y;
 
-// need loop!
-	if (x == mx) { // if (x > y && x > z)
-		cout << "The first is rich\n";
-		if (y == mn) // if ( y < x && y < z)
-			cout << "The third is midle\n" << "The second is poor";
-		else
-			cout << "The second is midl\n" << "The third is poor";
-	}
-	else if (y == mx) {// if (y > y && y > z)
-		cout << "The second is rich\n";
-		if (x == mn)
-			cout << "The third is midl\n" << "The first is poor";
-		else
-			cout << "The first is midle\n" << "The third is poor";
-	}
-	else {// if (z > x && z > y)
-		cout << "The third is rich\n";
-		if (y == mn)
-			cout << "The first is midle\n" << "The second is poor";
-		else
-			cout << "The second is modl\n" << "The first is poor";
-	}
+	if (y - x == 2 || x + y == 10)
+		cout << "Good";
 }
-		
-	//	Don't work:
-// 	   case x - need constant
-// switch (max1) {
-//		case x:
-//			cout << "x"
-//			break;
-//		case y:
-//			cout << "y";
-//			break;
-//		case z:
-//			cout << "z";
-//			break;
-//		default:
-//				cout << "errr";
-//  }
