1622f0f174942bad84cc58faa75037e6f95d9fd5
Yhoo2 <yhooi2011@gmail.com>
2023-08-15 08:44:52 +0300

    Lesson 7.3 Calculator <switch>, <case>, <break>, <default>.

--- C++/Lesson4/Lesson4.cpp
+++ C++/Lesson4/Lesson4.cpp
@@ -3,26 +3,25 @@
 using namespace std;
 
 int main() {
-	int x, y;
+	double x, y;
 	char oper;
 	cin >> x >> oper >> y;
 
 	cout << "Operation result: ";
-	if (oper == '+') {
-		cout << x + y;
+	switch (oper) {
+		case '+':
+			cout << x + y;
+			break;
+		case '-':
+			cout << x - y;
+			break;
+		case '*':
+			cout << x * y;
+			break;
+		case '/':
+			cout << x / y;
+			break;
+		default:
+			cout << "No such operator";
 	}
-	else if (oper == '-') {
-		cout << x - y;
-	}
-	else if (oper == '*') {
-		cout << x * y;
-	}
-	else if (oper == '/') {
-		cout << x / y;
-	}
-	else if (oper == '%') {
-		cout << x % y;
-	}
-	else
-		cout << "ERROR";
 }
