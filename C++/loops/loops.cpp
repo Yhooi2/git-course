3f646f749b247c30be0a6193a895447a055d6aa0
Yhoo2 <yhooi2011@gmail.com>
2023-08-16 17:24:56 +0300

    Lesson 10.1 own version. Fibonacci nomber & Ternary operator

--- C++/loops/loops.cpp
+++ C++/loops/loops.cpp
@@ -4,9 +4,16 @@ using namespace std;
 
 int main()
 {
-	int coint = 1;
-	cin >> coint;
-	while (coint % 10 == 0 && coint != 0)
-		coint /= 10;
-	cout << coint;
+	int count;
+	int phiPrev = 1;
+	int phiNext = 1;
+	cin >> count; 
+	while (phiNext < count) 
+	{
+		phiNext = phiNext + phiPrev; // 1 + 1, 2 + 1, 3 + 2...
+		phiPrev = phiNext - phiPrev; // 2 - 1 = 1, 3 - 1 = 2...
+		cout << phiNext << ' ' << phiPrev << endl;
+
+	}
+	cout << count << ((phiNext == count) ? " - it's fibonacci nomber" : " - it isn't fibonacci nomder");
 }
