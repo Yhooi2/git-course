71615d4d839b62831c23e13ba4ab5cd7453439a9
Yhoo2 <yhooi2011@gmail.com>
2023-08-16 19:19:29 +0300

    Lesson 10.2 do while, +=

--- C++/loops/loops.cpp
+++ C++/loops/loops.cpp
@@ -5,13 +5,10 @@ using namespace std;
 int main()
 {
 	int count;
-	int phiPrev = 1;
-	int phiNext = 1;
-	cin >> count; 
-	while (phiNext < count) 
-	{
-		phiNext = phiNext + phiPrev; // 1 + 1, 2 + 1, 3 + 2...
-		phiPrev = phiNext - phiPrev; // 2 - 1 = 1, 3 - 1 = 2...
-	}
-	cout << count << ((phiNext == count) ? " - it's fibonacci number" : " - it isn't fibonacci numder");
+	int sum = 0;
+	do {
+		cin >> count;
+		sum += count;
+	} while (count != 0);
+	cout << sum;		
 }
