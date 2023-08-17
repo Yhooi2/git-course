f4637363a51696b1ba8534dd8b2ed27b52d4a33b
Yhoo2 <yhooi2011@gmail.com>
2023-08-17 12:46:41 +0300

    Lesson 10.4 smallest devisor v0

--- C++/loops/loops.cpp
+++ C++/loops/loops.cpp
@@ -4,30 +4,14 @@ using namespace std;
 
 int main()
 {
-	int min, end, left, right;
-	int count = 0;
-	int counter = 0;
-	int mx = 0;
-	cin >> end;// 7
-	for (int i = 1; i <= end; i++)
-		// 1;2;3;4;5;6;7;8...
+	long long number;
+	cin >> number;
+	for (int i = 2; i <= number; i++)
 	{
-		min = count; 
-		// min = 0;1;2;1;2;3;4;1...
-		cin >> count;
-		// input count = 1;2;1;2;3;4;1...
-		min < count ? ++counter : counter = 1;
-		// if 0 < count; conter = +1; else conter = 1;   1; 2; 1; 2; 3; 4; 1...
-		if (mx < counter)
-			// 0 < 1; 1 < 2; 2 < 1; 2 < 2; 2 < 3; 3 < 4; 3 <1...
+		if (number % i == 0)
 		{
-			left = i + 1 - counter;
-			// 1; 2; -; -; 5 + 1 - 3 = 3; 3; -...
-			right = i;
-			// = 1; 2; -; -; 5; 6; - ...
+			cout << i;
+			return 0;
 		}
-		mx = max(mx, counter);
-		// = 1; 2; 2; 3; 4; 4...
-	} 
-	cout << mx << ' ' << left << ' ' << right;
+	}
 }
