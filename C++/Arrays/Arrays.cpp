501a7bcdc1084c4394acc4b19ef61292926f5bf8
Yhoo2 <yhooi2011@gmail.com>
2023-08-19 05:08:58 +0300

    Lesson 11.2 <algorithm>: sort, reverse, unoque, auto

--- C++/Arrays/Arrays.cpp
+++ C++/Arrays/Arrays.cpp
@@ -1,30 +1,20 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
-<<<<<<< HEAD
 int arr[50];
+int n;
 
 int main() {
-	int n,near, mn = 9e5;
 	cin >> n;
-	for (int i = 0; i < n; i++) {
-		cin >> arr[i];
-		if (abs(n - arr[i]) < mn)
-		{
-			mn = abs(n - arr[i]);
-			near = arr[i];
-		}
-	}
-	cout << near;
-=======
-int main() {
-	int n = 5;
-	//cin >> n;
-	int arr[] = { 1, 2, 3, 4, 5 };
-	//for (int i = 0; i < n; i++)
-		//cin >> arr[i];
-	for (int i = n - 1; i >= -100; i--)
-		cout << arr[i] << ' ';
->>>>>>> 3a018ae4bb0b5ef2bd773e302f59e823ab44f42c
+	for (int i = 0; i < n; i++)
+			cin >> arr[i];
+
+	sort(arr, arr + n);
+	reverse(arr, arr + n);
+	auto adress = unique(arr, arr + n);
+	for (int i = 0; i < adress - arr; i++)
+		cout << arr[i];
+
 }
