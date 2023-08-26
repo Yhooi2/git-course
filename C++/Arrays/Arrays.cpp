0d9ada7bca81c00d79546f0cdcefec3542e32f24
Yhoo2 <yhooi2011@gmail.com>
2023-08-26 10:11:17 +0300

    Lesson 12.0 Array inside array. for loops inside for loops. Average, sort, reverse, find and sum. switch case

--- C++/Arrays/Arrays.cpp
+++ C++/Arrays/Arrays.cpp
@@ -4,16 +4,64 @@
 using namespace std;
 
 int arr[50];
-int n; 
-int main() {
-	cin >> n;
-	for (int i = 0; i < n; i++)
-			cin >> arr[i];
 
-	sort(arr, arr + n);
-	reverse(arr, arr + n);
-	auto adress = unique(arr, arr + n);
-	for (int i = 0; i < adress - arr; i++)
-		cout << arr[i];
+int main()
+{
+	int number_digits, number_operations, begin, end, change;
+	char operation;
 
+	cout << "\nEnter number of digits: ";
+	cin >> number_digits;
+	cout << "\n Enter digits: ";
+	for (int i = 1; i <= number_digits; i++)
+		cin >> arr[i];
+	cout << "\nEnter number of operations: ";
+	cin >> number_operations;
+	for (int i = 1; i <= number_operations; i++)
+	{
+		cout << "\nEnter operation (+ -sum,a-average,m-max,s-sort,r-reverse,f-find): ";
+		cin >> operation;
+		cout << "\nEnter begining and end: ";
+		cin >> begin >> end;
+		switch (operation)
+		{
+		case '+'://sum
+			change = 0;
+			for (int j = begin; j <= end; j++)
+				change += arr[j];
+			cout << change << endl;
+			break;
+		case 'a'://average
+			change = 0;
+			for (int j = begin; j <= end; j++)
+				change += arr[j];
+			cout << double(change) / double(end - begin + 1) << endl;
+			break;
+		case 'm'://max
+			change = 0;
+			for (int j = begin; j <= end; j++)
+				change = max(arr[j], change);
+			cout << change << endl;
+			break;
+		case 's'://sort
+			sort(arr + begin, arr + end +1);
+			for (int j = begin; j <= end; j++)
+				cout << arr[j];
+			break;
+		case 'r'://reverse
+			reverse(arr + begin, arr + end +1);
+			for (int j = begin; j <= end; j++)
+				cout << arr[j];
+			break;
+		case'f': //find
+			cout << "\nEnter number for find: ";
+			cin >> change;
+			for (int j = begin; j <= end; j++)
+				if (change == arr[j])
+				{
+					cout << j;
+					break;
+				}
+		}		
+	}
 }
