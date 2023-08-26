09b1228fec72cbc91bac9081bcfbc72c4c321328
Yhoo2 <yhooi2011@gmail.com>
2023-08-26 12:43:55 +0300

    Lesson 12.1 Catalog. struct. do while

--- C++/Arrays/Arrays.cpp
+++ C++/Arrays/Arrays.cpp
@@ -3,65 +3,35 @@
 
 using namespace std;
 
-int arr[50];
+struct Numbers
+{
+	int number;
+	string name;
+};
 
+Numbers catalog[100];
 int main()
 {
-	int number_digits, number_operations, begin, end, change;
-	char operation;
-
-	cout << "\nEnter number of digits: ";
-	cin >> number_digits;
-	cout << "\n Enter digits: ";
-	for (int i = 1; i <= number_digits; i++)
-		cin >> arr[i];
-	cout << "\nEnter number of operations: ";
-	cin >> number_operations;
-	for (int i = 1; i <= number_operations; i++)
+	// input directory
+	int size;
+	cout << "Enter size of catalog: ";
+	cin >> size;
+	for (int i = 0; i < size; i++)
 	{
-		cout << "\nEnter operation (+ -sum,a-average,m-max,s-sort,r-reverse,f-find): ";
-		cin >> operation;
-		cout << "\nEnter begining and end: ";
-		cin >> begin >> end;
-		switch (operation)
-		{
-		case '+'://sum
-			change = 0;
-			for (int j = begin; j <= end; j++)
-				change += arr[j];
-			cout << change << endl;
-			break;
-		case 'a'://average
-			change = 0;
-			for (int j = begin; j <= end; j++)
-				change += arr[j];
-			cout << double(change) / double(end - begin + 1) << endl;
-			break;
-		case 'm'://max
-			change = 0;
-			for (int j = begin; j <= end; j++)
-				change = max(arr[j], change);
-			cout << change << endl;
-			break;
-		case 's'://sort
-			sort(arr + begin, arr + end +1);
-			for (int j = begin; j <= end; j++)
-				cout << arr[j];
-			break;
-		case 'r'://reverse
-			reverse(arr + begin, arr + end +1);
-			for (int j = begin; j <= end; j++)
-				cout << arr[j];
-			break;
-		case'f': //find
-			cout << "\nEnter number for find: ";
-			cin >> change;
-			for (int j = begin; j <= end; j++)
-				if (change == arr[j])
-				{
-					cout << j;
-					break;
-				}
-		}		
+		cout << "\nEnter phone number " << i+1 << ": ";
+		cin >> catalog[i].number;
+		cout << "\nEnter name " << i+1 << ": ";
+		cin >> catalog[i].name;
 	}
+	// Search the cataloge for a name by phone number
+	int look;
+	do
+	{
+		cout << "\nEnter number to look for (0 to exit): ";
+		cin >> look;
+		for (int i = 0; i < size; i++)
+			if (look == catalog[i].number)
+				cout << catalog[i].name;
+	} while (look != 0);
+	return 0;
 }
