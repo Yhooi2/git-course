cc2b259d7784e10e811ef84c1eaae57c57672e9c
Yhoo2 <yhooi2011@gmail.com>
2023-08-27 18:51:42 +0300

    :Lesson 14 Tic-Tac-Toe game. system(cls), bool flags. |= bool | and Ternury operator

--- C++/Arrays/Arrays.cpp
+++ C++/Arrays/Arrays.cpp
@@ -1,30 +1,92 @@
 #include <iostream>
 
-const int MAX = 100;
+const int SIZE = 3; // size of the board
 
 using namespace std;
 
 
 int main() {
 
-	int matrix[MAX][MAX];//int arr[2][2][2]; {{{ , } { , }}{{ , }{ , }}}
-	int sizeColumns, sizeRows;
+	char board[SIZE][SIZE];
 
-	cin >> sizeColumns // Enter matrix size columns
-		>> sizeRows; // Enter matrix size rows
-
-	// input matrix elements
-	for (int i = 0; i < sizeColumns; i++) {
-		for (int j = 0; j < sizeRows; j++) {
-			cin >> matrix[i][j];
+	// Initialize empty board
+	for (int i = 0; i < SIZE; i++) {
+		for (int j = 0; j < SIZE; j++) {
+			board[i][j] = '_';
 		}
 	}
 
-// Print matrix trunspose
-	for (int i = 0; i < sizeRows; i++) {
-		for (int j = 0; j < sizeColumns; j++) {
-			cout << matrix[j][i] << ' ';
+	// Start game
+	for (int torn = 0; torn < 9; torn++) {
+		int x, y;
+		char sign;
+
+		do {
+			system("cls");
+			// Print board
+			cout << "  1 2 3\n";
+			for (int i = 0; i < SIZE; i++) {
+				cout << i+1 << " ";
+				for (int j = 0; j < SIZE; j++) {
+					cout << board[i][j] << ' ';
+				}
+				cout << endl;
+			}
+
+		 // Get player move
+			cout << endl << "Player " << ((torn % 2 == 0) ? "1 " : "2 ") << "Enter coordinates : ";
+			cin >> x >> y;
+			x--; y--;
+
+			if (torn % 2 == 0)
+				sign = 'X';
+			else
+				sign = 'O';
+		} while (board[x][y] != '_');//check correct
+		// Input date
+		board[x][y] = sign;
+
+		// check result	
+		bool check = false;
+		bool checkDioganal = true;
+		bool check2Dioganal = true;
+
+		for (int i = 0; i < SIZE; i++) {
+			bool checkColumns = true;
+			bool checkRows = true;
+
+			for (int j = 0; j < SIZE; j++) {
+				if (board[i][j] != sign)
+					checkColumns = false;
+				if (board[j][i] != sign)
+					checkRows = false;
+			}
+			check |= checkColumns | checkRows;
+
+			if (board[i][i] != sign)
+				checkDioganal = false;
+			if (board[i][2-i] != sign)
+				check2Dioganal = false;
 		}
-		cout << endl;
+		check |= checkDioganal | check2Dioganal;
+
+		if (check)
+		{
+			system("cls");
+			// Outpute board
+			cout << "  1 2 3\n";
+			for (int i = 0; i < SIZE; i++) {
+				cout << i + 1 << " ";
+				for (int j = 0; j < SIZE; j++) {
+					cout << board[i][j] << ' ';
+				}
+				cout << endl;
+			}
+			// Outpute result
+			cout << endl << "Win Player " << ((torn % 2 == 0) ? "1 " : "2 ") << "!!!\n";
+			return 0;
+		}
+
 	}
+	cout << "Draw!";
 }
