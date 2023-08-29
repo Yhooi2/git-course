9e4676818d98d3f9c75e0eeccd3055c3771c569f
Yhoo2 <yhooi2011@gmail.com>
2023-08-29 21:05:03 +0300

    Lesson 18 For through recursion, revers array, sum Digits, cat array, find fibonacci nomber

--- C++/Functions/Functions.cpp
+++ C++/Functions/Functions.cpp
@@ -1,78 +1,75 @@
-#include<iostream>
+#include <iostream>
 
 const int SIZE = 100;
 
 using namespace std;
 
-//Input Array
-void enterArray(int arr[SIZE], const int& n) {
-
-	if (n < 0) return;
-	cin >> arr[n];
-	enterArray(arr, n - 1);
-
-}
-
-// Print Arroy
-void printArray(int(arr)[SIZE], const int& n) {
-
-	if (n < 0) {
-		cout << endl;
-		return;
-	}
-	cout << arr[n] << ' ';
-	printArray(arr, n - 1);
-}
-
-// buble sort
-void sortArray(int arr[SIZE],const int& n) {
+// Enter array
+void funcFor(int arr[], int start, int end, int step = 1) {
 	
-	bool flage = true;
-	do {
-		flage = false;
-		for (int i = 0; i < n; i++) {
-			if (arr[i] < arr[i + 1]) {
-				swap(arr[i], arr[i + 1]);
-				flage = true;
-			}
-		}
-	} while (flage); 
-}
-
-// conter unique nombers and 
-int unique(int arr[SIZE], const int& n) {
-	int counter = 0;
-
-	for (int i = 0; i < n; i++) {
-		if (arr[i] != arr[i + 1]) {
-			counter++;
-			arr[counter] = arr[i + 1];
-		}
-	}
-	return counter;
-}
-
-// revers Array
-void reversArray(int arr[SIZE], const int& n) {
-	
-	for (int i = 0; i < n / 2; i++) {
-		swap(arr[i], arr[n - i]);
-	}
+	cin >> arr[start];
+	if (start <  end) 
+		funcFor(arr, start + step, end); // recursion after
 	return;
+}
+
+// Print revers array
+void printReversFor(int arr[], int start, int end, int step = 1) {
+	
+	if (start < end)
+	printReversFor(arr, start + step, end); // recursion before
+	cout << arr[start] << ' ';
+	return;
+}
+
+// Sum array
+int sumArray(int arr[], int start, int count) {
+
+	if (count < start) return 0; // recursion sum
+	return arr[count] + sumArray(arr, start, count - 1);
+}
+
+// Sum Digits
+int sumDigits(int n) {
+	if (n == 0) return 0;
+	return n % 10 + sumDigits(n / 10);
+}
+
+// find fibonacci nomber
+int fibonacci(int n, int x = 1, int y = 1) {
+	if (n == 0) return y;
+	return fibonacci(n - 1, y, x + y);
+	//1 1 2 3 5 8 13 21 34 55 89 144 233 377 610 987 1597
+	//0 1 2 3 4 5  6  7  8  9 10  11  12  13  14  15  16
 }
 
 int main() {
 
 	int arr[SIZE];
-	int countity;
-	cin >> countity;
-	countity--; // becouse index array start with 0
+	int start, end;
 
-	enterArray(arr, countity);
-	sortArray(arr, countity);
-	printArray(arr, countity);
+	cout << "Enter size array: ";
+	cin >> end;
+	end--;
 
-	int newSize = unique(arr, countity);
-	reversArray(arr, newSize);
-	printArray(arr, newSize);
+	// Enter array
+	cout << "Inpot array: ";
+	funcFor(arr, 0, end); 
+	cout << endl << "Enter start and finish array: ";
+	cin >> start >> end;
+	end--;
+
+	// Print cat revers array
+	printReversFor(arr, start, end);
+	cout << endl;
+
+	// Print sum cat array
+	cout << sumArray(arr, start, end) << endl;
+
+	// Print sum digits of sum cat array
+	int sum = sumDigits(sumArray(arr, start, end));
+	cout <<"Sum digits:\n" << sum << endl;
+
+	// Print Fibonacci nomber this sum
+	cout << "Fibonacci nomber: " << fibonacci(sum - 1) << endl;
 }
