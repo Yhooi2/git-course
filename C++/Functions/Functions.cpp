1fef7bfc7c52acb0c41066385bcc59e9e07ef690
Yhoo2 <yhooi2011@gmail.com>
2023-08-30 13:44:14 +0300

    Lesson 18 improve

--- C++/Functions/Functions.cpp
+++ C++/Functions/Functions.cpp
@@ -5,38 +5,38 @@ const int SIZE = 100;
 using namespace std;
 
 // Enter array
-void funcFor(int arr[], int start, int end, int step = 1) {
-	
+void funcFor(int arr[], const int& start, const int& end, int step = 1) {
+
 	cin >> arr[start];
-	if (start <  end) 
+	if (start < end)
 		funcFor(arr, start + step, end); // recursion after
 	return;
 }
 
 // Print revers array
-void printReversFor(int arr[], int start, int end, int step = 1) {
-	
+void printReversFor(int arr[], const int& start, const int& end,const int& step = 1) {
+
 	if (start < end)
-	printReversFor(arr, start + step, end); // recursion before
+		printReversFor(arr, start + step, end); // recursion before
 	cout << arr[start] << ' ';
 	return;
 }
 
 // Sum array
-int sumArray(int arr[], int start, int count) {
+int sumArray(int arr[], const int& start, const int& count) {
 
 	if (count < start) return 0; // recursion sum
 	return arr[count] + sumArray(arr, start, count - 1);
 }
 
 // Sum Digits
-int sumDigits(int n) {
+int sumDigits(const int& n) {
 	if (n == 0) return 0;
 	return n % 10 + sumDigits(n / 10);
 }
 
 // find fibonacci nomber
-int fibonacci(int n, int x = 1, int y = 1) {
+int fibonacci(const int& n, int x = 1, int y = 1) {
 	if (n == 0) return y;
 	return fibonacci(n - 1, y, x + y);
 	//1 1 2 3 5 8 13 21 34 55 89 144 233 377 610 987 1597
@@ -54,7 +54,7 @@ int main() {
 
 	// Enter array
 	cout << "Inpot array: ";
-	funcFor(arr, 0, end); 
+	funcFor(arr, 0, end);
 	cout << endl << "Enter start and finish array: ";
 	cin >> start >> end;
 	end--;
@@ -68,7 +68,7 @@ int main() {
 
 	// Print sum digits of sum cat array
 	int sum = sumDigits(sumArray(arr, start, end));
-	cout <<"Sum digits:\n" << sum << endl;
+	cout << "Sum digits:\n" << sum << endl;
 
 	// Print Fibonacci nomber this sum
 	cout << "Fibonacci nomber: " << fibonacci(sum - 1) << endl;
