83501b42ae1114b47955f56b7fe241fc5e913e0a
Yhoo2 <yhooi2011@gmail.com>
2023-09-01 13:24:32 +0300

    Lesson 19.2 permutation with repitition

--- C++/Functions/Functions.cpp
+++ C++/Functions/Functions.cpp
@@ -5,17 +5,18 @@ using namespace std;
 
 const int SIZE = 100;
 string s;
-bool flage[SIZE];
+int flage[SIZE];
 
 void permutation(const int& n, const string& temp) {
+
 	if (s.size() == n)
 		cout << temp << endl;
 
-	for (int i = 0; i < s.size(); i++) {
-		if (!flage[i]) {
-			flage[i] = true;
-			 permutation(n + 1, temp + s[i]);
-			flage[i] = false;
+	for (int i = 0; i < 26; i++) {
+		if (flage[i] > 0) {
+			--flage[i];
+			 permutation(n + 1, temp + (char)((int)'a' + i));
+			++flage[i];
 		}			
 	}
 	return;
@@ -28,6 +29,9 @@ int main() {
 	cin >> s;
 	sort(s.begin(), s.end());
 
+	for (int i = 0; i < s.size(); i++) {
+		++flage[s[i] - 'a'];
+	}
 	permutation(0, "");
 }
 
