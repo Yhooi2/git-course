e50480091b2fc0c11261a048430cec6edef02eec
Yhoo2 <yhooi2011@gmail.com>
2023-09-05 13:11:50 +0300

    Lesson 1 <string> getline(), to_string(). <fstream> ifstream name(file, ios_base::trunc) and ofstream name(file, ios_base::app). name.open(file), name.close(), insert

--- C++/Functions/Functions.cpp
+++ C++/Functions/Functions.cpp
@@ -1,15 +1,40 @@
 #include <iostream>
-#include <string>
+#include <fstream> // ofstream(), ifistram()
+#include <string> // getline(), to_string()
 
 int main() {
-	int n = 22222;
-	std::string s,
-		s2 = std::to_string(n);
 
-	std::cin >> s;
-	s.erase(s.size() - 2);//, s.size());
-	s.insert(s.size(), "em");
-	std::cout << (s > s2) << std::endl;
-	std::cout << std::endl << s << ' ';
-	std::cout << s2.substr(0, 3) << ' ' << s2;//1, 2)
+	//freopen - learn
+
+	std::ofstream fout("output.txt", std::ios_base::trunc);
+	fout.close();
+	int counter = 0;
+	std::string s, n;
+
+	std::ifstream fin("input.txt");
+
+	if (fin.is_open()) {
+		while (getline(fin, s)) {
+			n = std:: to_string(++counter);
+			fout.open("output.txt", std::ios_base::app);
+			fout << n +" " + s + "\n";
+			fout.close();
+		}		
+	}
+	fin.close();
+
+	fout.open("output.txt", std::ios_base::app);
+	fout << "\n End";
+	fout.close();
+	fin.open("output.txt");
+	if(fin.is_open())
+		while (getline(fin, s)) {
+
+			std::cout << s << std::endl;
+		}
+
+	fin.close();
+	n.insert(n.size(), " -  string");
+	std::cout << n;// 
+
 }
