a382b3b78b0f8c02165ccddc41c8c76dac5c46a0
Yhoo2 <yhooi2011@gmail.com>
2023-09-09 22:16:51 +0300

    Lambda function Calculater, switch case

--- study/study.cpp
+++ study/study.cpp
@@ -1,31 +1,35 @@
 #include <iostream>
-#include <functional> // std::fanction<type(type of argumrnt)> name = name function; 
+#include <functional>
 
-// print int argument
-void print(int x) {
-	std::cout << x << std::endl;
+/*int multiplication(int x, int y) {
+	return x * y;
 }
+*/
 
-//f with argument point to function
-void useF(void(*point)()) {
-	point();
-}
-
-//funcktion with argument point function with argument int
-void useF(void(*point)(int)) {
-
-	point(3);
-}
-// Same thing
-void useF(std::function<void(int)> f) {
-
-	f(4);
+int calc(std::function<int(int, int)> f, int x, int y) {
+	return f(x, y);
 }
 
 int main() {
-
-	void(*point)(int) = print;//initialization point
-	useF(point);//f(f())
-	std::function<void(int)> pointf = print;
-	useF(pointf);
+	char oper;
+	int x, y;
+	std::cin >> x >> oper >> y;
+	std::function<int(int x, int y)> f;
+	std::cout << "Operation resalt: ";
+	switch (oper) {
+	case '*':
+		//f = multiplication;
+		f = [](int x, int y) {return x * y; };
+		break;
+	case '+':
+		f = [](int x, int y) {return x + y; };
+		break;
+	case '-':
+		f = [](int x, int y) {return x - y; }; // Lambda functions same thing
+		break;
+	case '/':
+		f = [](int x, int y) {return x / y; };
+	}
+	std::cout << calc(f, x, y);
+	
 }
