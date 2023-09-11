be8f6a3873ed016c48ef01ccd239c8ed4b6d4033
Yhoo2 <yhooi2011@gmail.com>
2023-09-11 23:09:42 +0300

    Type* pointer = &car, (*pointer).legs = pointer->legs, std::function<void(Type*)>name, name = [](Type* pointer) {}, function(std::function<void(Type*)>name, Type* pointer)

--- study/study.cpp
+++ study/study.cpp
@@ -2,18 +2,40 @@
 #include <functional>
 #include <algorithm>
 
+struct Animal {
+	int legs;
+	std::string name;
+};
+
+void setSetting(std::function<void(Animal*)> f1, Animal* p) {
+
+	f1(p);
+}
 /*
 bool srt(int x, int y) {
 	return x > y;
 }
 */
-int main() {
+	int main() {
 
-	int array[10] = { 1, 5, 4, 9, 7, 6, 0, 8 };
-	std::sort(array, array + 8, [](int x, int y) {return x > y; });
-	for (int i = 0; i < 8; i++) {
-		std::cout << array[i] << ' ';
+		Animal cat{ 4, "Barsic" };
+		Animal* pointer = &cat;
+		std::cout << sizeof(pointer) << std::endl;
+
+		std::cout << (*pointer).legs << ' ' << (*pointer).name << std::endl;// Same thing
+		std::cout << pointer->legs << ' ' << pointer->name << std::endl;// Same thing
+		std::cout << cat.legs << ' ' << cat.name << std::endl;// Same thing
+
+		std::string setting = "name";
+		std::function<void(Animal*)> f;
+		std::cout << &f << std::endl;
+		if (setting == "name") {
+			f = [](Animal* dog) {dog->name = "Djeck"; };// Lambda funcktion
+		}
+		else
+			f = [](Animal* dog) {dog->legs = 3; };
+
+		setSetting(f, pointer);// pointer of function for function
+		std::cout << pointer->legs << ' ' << pointer->name; 
+
 	}
-	
-
-}
