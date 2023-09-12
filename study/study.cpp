99675e0c1737fc11c4c07b27208de440742457e9
Yhoo2 <yhooi2011@gmail.com>
2023-09-12 16:46:19 +0300

    vector> vector<int> v(), vector<vector<int>( 19, vector<int>({1,2,3}) or push_back(vector<>())

--- study/study.cpp
+++ study/study.cpp
@@ -1,22 +1,22 @@
 #include <iostream>
-
-
+#include <vector>
 
 int main() {
-	int n = 0;
-	std::cin >> n;
-	int** pp = new int* [n]; // dynamic matrix
 
-	for (int i = 0; i < n; ++i) {
-		pp[i] = new int[n];
+	int size = 0;
+	std::cin >> size;
+	std::vector<int> v(10, 0);// New vector by 10 and zerroing it
+
+	// New matrix by 10x10 anf zerroing it
+	std::vector<std::vector<int>> m(10, std::vector<int>(10, 0));
+	// and etc...
+
+	// New matrix by 10x3 and fill it
+	std::vector<std::vector <int>>m1(10, std::vector<int>({ 1,2,3 }));
+
+	// old method 
+	std::vector<std::vector<int>> v1;
+	for (int i = 0; i > size; ++i) {
+		v1.push_back(std::vector<int>());
 	}
-
-	pp[1][1] = 1;
-	std::cout << pp[1][1];
-
-	for (int i = 0; i < n; ++i) {
-		delete[] pp[i];
-	}
-	delete[] pp;
-
 }
