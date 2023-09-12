39e36169ebbb1016253872f20d96c78fd362669a
Yhoo2 <yhooi2011@gmail.com>
2023-09-12 13:48:43 +0300

    Pointer arithmetic, dynamic array, reverse pointer

--- study/study.cpp
+++ study/study.cpp
@@ -1,41 +1,47 @@
 #include <iostream>
-#include <functional>
-#include <algorithm>
+#include <algorithm> // sort
 
-struct Animal {
-	int legs;
-	std::string name;
-};
+void reverse(int* begin, int* end) {
+	
+	std::cout << "My reverse\n";
 
-void setSetting(std::function<void(Animal*)> f1, Animal* p) {
+	while(begin < end) {
+		//int* temp = begin;
+		//begin = end;
+		//end = temp;
+		std::swap(begin, end);
 
-	f1(p);
-}
-/*
-bool srt(int x, int y) {
-	return x > y;
-}
-*/
-	int main() {
-
-		Animal cat{ 4, "Barsic" };
-		Animal* pointer = &cat;
-		std::cout << sizeof(pointer) << std::endl;
-
-		std::cout << (*pointer).legs << ' ' << (*pointer).name << std::endl;// Same thing
-		std::cout << pointer->legs << ' ' << pointer->name << std::endl;// Same thing
-		std::cout << cat.legs << ' ' << cat.name << std::endl;// Same thing
-
-		std::string setting = "name";
-		std::function<void(Animal*)> f;
-		std::cout << &f << std::endl;
-		if (setting == "name") {
-			f = [](Animal* dog) {dog->name = "Djeck"; };// Lambda funcktion
-		}
-		else
-			f = [](Animal* dog) {dog->legs = 3; };
-
-		setSetting(f, pointer);// pointer of function for function
-		std::cout << pointer->legs << ' ' << pointer->name; 
-
+		begin++;
+		end--;
 	}
+
+	return;
+}
+
+int main() {
+	int size;
+	std::cin >> size;
+	int* diArray = new int[size];// dynamic array
+	//std::fill(diArray, diArray + size, size);
+
+	for (int i = 0; i < size; ++i) {
+		std::cin >> diArray[i];
+	}
+
+	std::sort(diArray, diArray + size);
+	reverse(diArray, diArray + size);
+
+	std::cout << diArray << ' ' << diArray[0] << ' ' << *diArray << ' ' << diArray + 1 <<  std::endl;
+	int* pointer = diArray;
+	do {
+		std::cout << *pointer << ' ' << pointer << std::endl;
+		++pointer;// Pointer arithmetic
+
+	} while (pointer < diArray + size);
+
+	int* first = diArray;
+	int* last = diArray + size;
+	std::cout << last - first;
+
+	delete[] diArray;
+}
