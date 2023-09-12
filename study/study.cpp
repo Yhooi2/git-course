a467b2a94faac1e8b6f964b2b621012c5a855681
Yhoo2 <yhooi2011@gmail.com>
2023-09-12 15:09:38 +0300

    Vector <vector> vector<int> v; v.push_back(number), v. pop_back(), <algorithm> std::sort(v.begin(), v.end());

--- study/study.cpp
+++ study/study.cpp
@@ -1,47 +1,23 @@
 #include <iostream>
-#include <algorithm> // sort
-
-void reverse(int* begin, int* end) {
-	
-	std::cout << "My reverse\n";
-
-	while(begin < end) {
-		//int* temp = begin;
-		//begin = end;
-		//end = temp;
-		std::swap(begin, end);
-
-		begin++;
-		end--;
-	}
-
-	return;
-}
+#include <vector>
+#include <algorithm>
 
 int main() {
-	int size;
+	int size = 0;
 	std::cin >> size;
-	int* diArray = new int[size];// dynamic array
-	//std::fill(diArray, diArray + size, size);
-
+	std::vector<int> v;//int* dynamicArray = new int[size];
+	int temp = 0;
 	for (int i = 0; i < size; ++i) {
-		std::cin >> diArray[i];
+		std::cin >> temp;
+		v.push_back(temp);
 	}
-
-	std::sort(diArray, diArray + size);
-	reverse(diArray, diArray + size);
-
-	std::cout << diArray << ' ' << diArray[0] << ' ' << *diArray << ' ' << diArray + 1 <<  std::endl;
-	int* pointer = diArray;
-	do {
-		std::cout << *pointer << ' ' << pointer << std::endl;
-		++pointer;// Pointer arithmetic
-
-	} while (pointer < diArray + size);
-
-	int* first = diArray;
-	int* last = diArray + size;
-	std::cout << last - first;
-
-	delete[] diArray;
+	std::sort(v.begin(), v.end()); //sort vector! dynamic array with pointer: sort(diArray, diArray + size);
+	v.push_back(1);
+	v.push_back(2);
+	v.pop_back();
+	
+	for (int i = 0; i < v.size(); ++i) {
+		std::cout << v[i] << ' ';
+	}
+	//delete dynamicArray
 }
