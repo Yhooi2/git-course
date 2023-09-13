17718cfbbb5c3458adfc1a1b3f4ad0de326371e8
Yhoo2 <yhooi2011@gmail.com>
2023-09-13 16:03:14 +0300

    <string> getlune(std::cin, s) <fstream> std::ofstream( std::ios_base::app)

--- study/study.cpp
+++ study/study.cpp
@@ -1,32 +1,13 @@
 #include <iostream>
-
-struct Node {
-	int value = 0;
-	Node* next = nullptr;
-};
+#include <fstream>
+#include <string>// getline
 
 int main() {
-	
-	Node* firstList = new Node;
-	Node* lastList = firstList; 
-	int n;
-	std::cin >> n;
-	int number = 0;
-	for (int i = 0; i < n; ++i) {
-		std::cin >> number;
-		lastList->value = number;
-		Node* newList = new Node;
-		lastList->next = newList;
-		lastList = newList;
-	}
+	std::string s;
+	std::getline(std::cin, s);
 
-	for (Node* iterator = firstList; iterator != lastList; iterator = iterator->next) {
-		std::cout << iterator->value;
-	}
+	std::ofstream fout("output.txt", std::ios_base::app);
 
-	for (auto iterator = firstList, it = iterator->next; iterator != lastList; iterator = it) {
-		it = iterator->next;
-		delete iterator;
-	}
-	delete lastList, firstList;
+	fout << s + "\n";
+	fout.close();
 }
