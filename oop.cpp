07673b2ffdce61dd0ed8b91eb047ad2926b1b4b2
Yhoo2 <yhooi2011@gmail.com>
2023-09-14 23:22:19 +0300

    Own Copy Constructor Entity(conts Entity& addres) : pointer(new int(*address.pointer)) {}

--- oop.cpp
+++ oop.cpp
@@ -1,43 +1,32 @@
 #include <iostream>
-#include <string>
- 
-class Animal {
-  
-  private:
- 
-	int legs = 4;
-	std::string name = "";
-	std::string type = "";
-	
-  public:
-	  Animal() {//Difoult Constructor
-		  std::cout << "Difoult Constructor\n";
-	  }
 
-	  // Constructor with initializer
-	  Animal(int legs, std::string name, std::string type) : legs(legs), name(name), type(type) {} 
+class Entity {
 
-	void setLegs(int newLegs) { // Setter
-		if (newLegs >= 0 && newLegs <= 1000) {
-			legs = newLegs;
-		}
+public:
+	int* pointer;
+
+	// Default Constructor
+	Entity() : pointer(new int(2)) {} // Initializer list
+
+	// Copy Constryctor
+	Entity(const Entity& address) : pointer(new int(*address.pointer)) {} // Initializer list
+
+	// Destructor delite memery
+	~Entity() {
+
+		std::cout << "Destructor delete: "<< ' ' << pointer << '\n';
+		delete pointer;
+
 	}
 
-	std::string getLegs() { //Gettor
-		return std::to_string(legs) + ' ' + name + ' ' + type + '\n';
-	}
-	~Animal() { //Destructor
-
-		std::cout << "Work of destructor\n";
-	}
-	
 };
 
 int main() {
 
-	Animal cat(4, "Barsic", "cat");
-	Animal dog;
-	Animal car(cat); // copy 
+	Entity Any;
+	Entity Other(Any);
+	
+	// Pointers dereference
+	std::cout << *Any.pointer << ' ' << *Other.pointer << '\n';
 
-	std::cout << cat.getLegs() + dog.getLegs() + car.getLegs();
 }
