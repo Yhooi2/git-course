7932d028d726dd4b499554f720519386f6c051c6
Yhoo2 <yhooi2011@gmail.com>
2023-09-15 15:10:20 +0300

    Static type, Static Method, set, RFL and Initializer

--- oop.cpp
+++ oop.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <set>
 
 class Animal {
 
@@ -10,6 +11,7 @@ private:
 
 public:
 	static int countAnimal;
+	static std::set<std::string> nameAnimal;
 	
 	//Default Constructor
 	Animal() {
@@ -17,25 +19,33 @@ public:
 		std::cout << "Init Difoult Constructor\n";
 	}
 
+	// Constructor for name
+	Animal(std::string name) : name(name) {
+		nameAnimal.insert(name);
+		++countAnimal;
+		std::cout << "Used Constructor for name\n";
+	}
 	// Parameters Constructor and Initializer 
 	Animal(int legs, std::string name, std::string type) : legs(legs), name(name), type(type) {
+		nameAnimal.insert(name);
 		++countAnimal;
 		std::cout << "Init Parameters Constructor\n";
 	}
 
 	// Copy Constructor and Initializer 
-	Animal(Animal& copy) : legs(copy.legs), name(copy.name), type(copy.type) {
+	Animal(const Animal& copy) : legs(copy.legs), name(copy.name), type(copy.type) {
+		nameAnimal.insert(name);
 		++countAnimal;
 		std::cout << "Init Copy Constructor\n";
 	}
 
-	// Method Setter
-	void setLegs(int newLegs) { 
-		if (newLegs >= 0 && newLegs <= 1000) {
-			legs = newLegs;
+	static std::string printName() { // static method
+		std::string temp = "";
+		for (auto& it : nameAnimal) {// for (auto it = nameAnimal.begin(); it != nameAnimal.end(); ++it) {
+			temp += it + '\n';
 		}
+		return temp;
 	}
-
 	// Method Gettor
 	std::string getLegs() {
 		return std::to_string(legs) + ' ' + name + ' ' + type  + '\n';
@@ -49,18 +59,17 @@ public:
 
 };
 int Animal::countAnimal = 0;
+std::set<std::string> Animal::nameAnimal;
 
 
 int main() {
 
 	Animal cat(4, "Barsic", "cat");
-
-	//std::cout << cat.getLegs() + dog.getLegs() + car.getLegs();
-	std::cout << cat.countAnimal << '\n';
-
-	Animal dog;
-	std::cout << dog.countAnimal << '\n';
-	Animal car(cat); // copy 
-	std::cout << car.countAnimal << '\n';
+	
+	std::cout << cat.countAnimal << '\n' << Animal::printName() << '\n';	
+	Animal dog("dog");
+	std::cout << dog.countAnimal << '\n' << Animal:: printName() << '\n';
+	Animal car("car");
+	std::cout << car.countAnimal << '\n' << Animal::printName() << '\n';
 
 }
