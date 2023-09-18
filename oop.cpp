b9166e138be684186fec54a5aaa472bf08df0bff
Yhoo2 <yhooi2011@gmail.com>
2023-09-18 17:19:24 +0300

    template inheritance class

--- oop.cpp
+++ oop.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 template <typename T>
 
@@ -9,7 +10,7 @@ public:
 
 	Position(T x, T y) : x(x), y(y){}
 
-	void print() {
+	virtual void print() const {
 		std::cout << "x = " << x << '\n';
 		std::cout << "y = " << y << '\n';
 	}
@@ -22,18 +23,29 @@ public:
 
 	Position(std::string x, std::string y) : x(x), y(y) {}
 
-	void print() {
+	virtual void print() const {
 		std::cout << x << '\n';
 		std::cout << y << '\n';
 	}
+};
 
+template <typename U>
+class Position3D : public Position<U> {
+public:
+	U z;
+	Position3D(U x, U y, U z) : Position<U>(x, y), z(z) {}
+
+	void print() const override {
+		this->Position <U>::print();
+		std::cout << "z = " << z << '\n';
+	}
 };
 
 int main() {
-	 /*Position<int> point(1, 2);
-	 point.print();*/
-
-	 Position <std::string> point("x = 1", "y =2");
+	 Position3D<int> point(1, 2, 3);
 	 point.print();
+
+	 //Position <std::string> pointed("x = 1", "y =2");
+	 //pointed.print();
 
 }
