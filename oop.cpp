d8c54d458bf83a50e6db5ea95a2ce6c20e0d5a34
Yhoo2 <yhooi2011@gmail.com>
2023-09-18 14:14:11 +0300

    template<> class Name <type>

--- oop.cpp
+++ oop.cpp
@@ -1,11 +1,39 @@
 #include <iostream>
 
-template <typename T, typename T2>
+template <typename T>
 
-T sum(T a, T2 b) {
-	return a + b;
-}
+class Position {
+public:
+	T x;
+	T y;
+
+	Position(T x, T y) : x(x), y(y){}
+
+	void print() {
+		std::cout << "x = " << x << '\n';
+		std::cout << "y = " << y << '\n';
+	}
+};
+template<>
+class Position<std::string> {
+public:
+	std::string x;
+	std::string y;
+
+	Position(std::string x, std::string y) : x(x), y(y) {}
+
+	void print() {
+		std::cout << x << '\n';
+		std::cout << y << '\n';
+	}
+
+};
 
 int main() {
-	std::cout << sum(2, 2.3);
+	 /*Position<int> point(1, 2);
+	 point.print();*/
+
+	 Position <std::string> point("x = 1", "y =2");
+	 point.print();
+
 }
